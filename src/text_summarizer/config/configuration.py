87dd8956a7cf89d390d8ef891a584a2a6f3a6b3e
Shivam Sharma <54909615+shivamsharma00@users.noreply.github.com>
2023-08-31 13:29:38 -0700

    Data Validation stage completed

--- src/text_summarizer/config/configuration.py
+++ src/text_summarizer/config/configuration.py
@@ -1,9 +1,11 @@
 from text_summarizer.constants import *
 from text_summarizer.utils.common import read_yaml, create_directories
-from text_summarizer.entity import DataIngestionConfig
+from text_summarizer.entity import (DataIngestionConfig, 
+                                    DataValidationConfig)
 
 
 class ConfigurationManager:
+
     def __init__(self,
                  config_filepath=CONFIG_FILE_PATH,
                  params_filepath=PARAMS_FILE_PATH,
@@ -13,6 +15,7 @@ class ConfigurationManager:
         self.parmas = read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
+
 
     def get_data_ingestion_config(self)-> DataIngestionConfig:
         
@@ -30,3 +33,18 @@ class ConfigurationManager:
         print("done")
         return data_ingestion_config
         
+        
+    def get_data_validation_config(self)-> DataValidationConfig:
+        
+        config = self.config.data_validation
+        # print(config)
+
+        create_directories([config.root_dir])
+        
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES
+        )
+        print("done")
+        return data_validation_config
