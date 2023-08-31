59ba4a38639d2c70e39bac06b54c6adf3df91492
Shivam Sharma <54909615+shivamsharma00@users.noreply.github.com>
2023-08-31 11:47:12 -0700

    Stage 1 data ingestion completed

--- src/text_summarizer/utils/common.py
+++ src/text_summarizer/utils/common.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from typing import Any
 
 
-@measure_annotations
+@ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
 
@@ -28,12 +28,12 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
-        raise ValueErro("yaml file is empty")
+        raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
 
 
-@measure_annotations
+@ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
     """Create directories
     
@@ -41,7 +41,21 @@ def create_directories(path_to_directories: list, verbose=True):
         path_to_directories (list): list of path of directories
         ignore_log (bool, optional): ignore if multiple dirs is to be created. Default to False.
     """
+    
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"Created directory at: {path}")
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+
+    Args:
+        path (Path): path of the file
+
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~{size_in_kb} KB"
